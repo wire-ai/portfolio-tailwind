2aa363684216655ca182534327def90c5d4eebbb
wire-ai <wire613@gmail.com>
2022-04-07 12:22:21 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: { center: true, padding: "16px" },
+    extend: {
+      colors: {
+        primary: "#14b8a6",
+        secondary: "#64748b",
+        dark: "#0f172a",
+        white: "#f8fafc",
+      },
+      screens: { "2xl": "1320px" },
+    },
+  },
+  plugins: [],
+};
